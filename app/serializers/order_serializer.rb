6e4c67324ee12a1108676b9e5e92940c328ad79b
Seb Gertz <sebgertz@gmail.com>
2018-08-26 00:51:25 +0000

    automatically backed up by learn

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,4 +1,4 @@
 class OrderSerializer < ActiveModel::Serializer
   attributes :id, :created_at
-  has_many :product, serializer: ProductOrderSerializer
+  has_many :products, serializer: ProductOrderSerializer
 end
